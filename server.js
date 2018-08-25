09347f8aa801b8a3bdf0cccd2912e520ea8b72bb
Taylor Jackson <tjack94@hotmail.com>
2018-08-25 14:59:09 -0600

    3002

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get('/*', (req, res)=>{
     res.sendFile(path.join(__dirname, 'build/index.html'));
 })
 
-const port = 80
+const port = 3002
 
 app.listen(port, () => {
     console.log(`Server listening at localhost:${port}`);
