cde106faa4ebfc5be80592ac4e670c70b75dbc52
Taylor Jackson <tjack94@hotmail.com>
2018-08-25 14:36:01 -0600

    updated port

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom';
@@ -21,4 +20,4 @@ ReactDOM.render(
 </Router>
 </Provider>
 , document.getElementById('root'));
-registerServiceWorker();
+
